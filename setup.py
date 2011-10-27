c5e311dd738d0d364b7931b58ef367cdeec57fcd
Armin Ronacher <armin.ronacher@active-4.com>
2011-10-27 20:03:52 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='libbattlelog',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    version='0.1',
+    url='http://github.com/mitsuhiko/libbattlelog',
+    py_modules=['libbattlelog'],
+    description='Accesses the Battlefield 3 Battlelog',
+    install_requires=['requests'],
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
